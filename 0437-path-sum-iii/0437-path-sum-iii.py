598fbebaf667c88d1c0fc553bfc338df87c16a10
Shaina Shilpi <98258789+ShainaShilpi@users.noreply.github.com>
2023-07-19 23:58:04 +0530

    Time: 62 ms (88.31%), Space: 17.8 MB (45.62%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def pathSum(self, root, targetSum):
+        self.count = 0
+        summ = 0
+        sumMap = defaultdict(int)
+
+        def traverseTree(curr, summ):
+            if not curr:
+                return
+            
+            summ += curr.val
+            
+            if summ == targetSum :
+                self.count += 1
+
+            if sumMap.get(summ - targetSum):
+                self.count += sumMap[summ - targetSum]
+
+            sumMap[summ] += 1
+            
+            traverseTree(curr.left, summ)
+            traverseTree(curr.right, summ)
+
+
+            if sumMap.get(summ):
+                sumMap[summ] -= 1
+
+
+        traverseTree(root, 0)
+        return self.count
