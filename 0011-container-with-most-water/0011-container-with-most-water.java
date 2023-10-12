84a2089f029b8be73a376166319c3ca980dfcc80
Shaina Shilpi <98258789+ShainaShilpi@users.noreply.github.com>
2023-10-13 00:17:44 +0530

    Time: 5 ms (47.10%), Space: 54.6 MB (99.13%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,18 +1,37 @@
 class Solution {
-  public int maxArea(int[] height) {
-    int ans = 0;
-    int l = 0;
-    int r = height.length - 1;
-
-    while (l < r) {
-      final int minHeight = Math.min(height[l], height[r]);
-      ans = Math.max(ans, minHeight * (r - l));
-      if (height[l] < height[r])
-        ++l;
-      else
-        --r;
-    }
-
-    return ans;
-  }
+  public int maxArea(int height[]) {
+    int maxarea = 0;
+    int n = height.length;
+    int leftIndex = 0;
+    int rightIndex = n - 1;
+    while (leftIndex < rightIndex) {
+        int minHeight = Math.min(height[leftIndex], height[rightIndex]);
+        maxarea = Math.max(maxarea, (rightIndex - leftIndex) * minHeight);
+        if(height[leftIndex] < height[rightIndex])
+            leftIndex = leftIndex + 1;
+        else
+            rightIndex = rightIndex - 1;
+        }
+    return maxarea;
 }
+}
+
+
+// class Solution {
+//   public int maxArea(int[] height) {
+//     int ans = 0;
+//     int l = 0;
+//     int r = height.length - 1;
+
+//     while (l < r) {
+//       final int minHeight = Math.min(height[l], height[r]);
+//       ans = Math.max(ans, minHeight * (r - l));
+//       if (height[l] < height[r])
+//         ++l;
+//       else
+//         --r;
+//     }
+
+//     return ans;
+//   }
+// }
