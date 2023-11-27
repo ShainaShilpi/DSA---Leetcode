8a42ccdbdbdc8f86206b26824840351ed180ab3d
Shaina Shilpi <98258789+ShainaShilpi@users.noreply.github.com>
2023-11-27 22:51:54 +0530

    Time: 9 ms (48.17%), Space: 41 MB (99.02%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,8 +1,8 @@
 class Solution {
   public String reverseWords(String s) {
-    StringBuilder sb = new StringBuilder(s).reverse(); // Reverse the whole string.
-    reverseWords(sb, sb.length());                     // Reverse each word.
-    return cleanSpaces(sb, sb.length());               // Clean up the spaces.
+    StringBuilder sb = new StringBuilder(s).reverse();
+    reverseWords(sb, sb.length());
+    return cleanSpaces(sb, sb.length());
   }
 
   private void reverseWords(StringBuilder sb, int n) {
@@ -10,27 +10,26 @@ class Solution {
     int j = 0;
 
     while (i < n) {
-      while (i < j || i < n && sb.charAt(i) == ' ') // Skip the spaces.
+      while (i < j || i < n && sb.charAt(i) == ' ')
         ++i;
-      while (j < i || j < n && sb.charAt(j) != ' ') // Skip the spaces.
+      while (j < i || j < n && sb.charAt(j) != ' ')
         ++j;
-      reverse(sb, i, j - 1); // Reverse the word.
+      reverse(sb, i, j - 1);
     }
   }
 
-  // Trim leading, trailing, and middle spaces
   private String cleanSpaces(StringBuilder sb, int n) {
     int i = 0;
     int j = 0;
 
     while (j < n) {
-      while (j < n && sb.charAt(j) == ' ') // Skip the spaces.
+      while (j < n && sb.charAt(j) == ' ')
         ++j;
-      while (j < n && sb.charAt(j) != ' ') // Keep non spaces
+      while (j < n && sb.charAt(j) != ' ')
         sb.setCharAt(i++, sb.charAt(j++));
-      while (j < n && sb.charAt(j) == ' ') // Skip the spaces.
+      while (j < n && sb.charAt(j) == ' ')
         ++j;
-      if (j < n) // Keep only one space.
+      if (j < n)
         sb.setCharAt(i++, ' ');
     }
 
