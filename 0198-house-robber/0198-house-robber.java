d253af393e001ea52cf8192e237a5a4ceb8c7cd6
Shaina Shilpi <98258789+ShainaShilpi@users.noreply.github.com>
2024-01-22 23:21:38 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (17.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -6,7 +6,6 @@ class Solution {
     if (n == 1)
       return nums[0];
 
-    // dp[i] := the maximum money of robbing nums[0..i]
     int[] dp = new int[n];
     dp[0] = nums[0];
     dp[1] = Math.max(nums[0], nums[1]);
